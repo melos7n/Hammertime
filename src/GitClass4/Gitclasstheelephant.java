76ad604491d9fb418773b2ff3ae1a7090f620ece
melos7n <ernest.melo88@gmail.com>
2022-06-01 21:19:44 -0400

    added a git class 04

---
+++ src/GitClass4/Gitclasstheelephant.java
@@ -0,0 +1,13 @@
+package GitClass4;
+
+public class Gitclasstheelephant {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("popculture");
+		
+		
+	}
+
+}
